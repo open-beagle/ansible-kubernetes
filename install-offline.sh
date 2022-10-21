f8be702a63aa7cd9acbd3378e6136336e30948e0
shucheng <shucheng@bd-apaas.com>
2022-10-21 09:54:08 +0800

    v1.24.7

--- install-offline.sh
+++ install-offline.sh
@@ -29,12 +29,20 @@ if ! [ -e /etc/kubernetes/ansible/ansible-kubernetes-images-$K8S_VERSION-$TARGET
 mkdir -p /etc/kubernetes/ansible
 # 下载文件
 # 安装镜像 ansible-kubernetes-images-v1.24.7-amd64.tgz 1526MB
-# 安装脚本 ansible-kubernetes-v1.24.7-amd64.tgz 276MB
 curl $HTTP_SERVER/k8s/ansible/$TARGET_ARCH/ansible-kubernetes-images-$K8S_VERSION-$TARGET_ARCH.tgz > /etc/kubernetes/ansible/ansible-kubernetes-images-$K8S_VERSION-$TARGET_ARCH.tgz
-curl $HTTP_SERVER/k8s/ansible/$TARGET_ARCH/ansible-kubernetes-$K8S_VERSION-$TARGET_ARCH.tgz > /etc/kubernetes/ansible/ansible-kubernetes-$K8S_VERSION-$TARGET_ARCH.tgz
+fi
 
+if ! [ -e /etc/kubernetes/ansible/ansible-kubernetes-$K8S_VERSION-$TARGET_ARCH.tgz ]; then
+mkdir -p /etc/kubernetes/ansible
+# 下载文件
+# 安装脚本 ansible-kubernetes-v1.24.7-amd64.tgz 276MB
+curl $HTTP_SERVER/k8s/ansible/$TARGET_ARCH/ansible-kubernetes-$K8S_VERSION-$TARGET_ARCH.tgz > /etc/kubernetes/ansible/ansible-kubernetes-$K8S_VERSION-$TARGET_ARCH.tgz
+fi
+
+if ! [ -e /etc/kubernetes/ansible/.ansible-kubernetes-$K8S_VERSION-$TARGET_ARCH ]; then
 # 加载镜像
 docker load -i /etc/kubernetes/ansible/ansible-kubernetes-$K8S_VERSION-$TARGET_ARCH.tgz
+touch /etc/kubernetes/ansible/.ansible-kubernetes-$K8S_VERSION-$TARGET_ARCH
 fi
 
 docker run \
@@ -43,5 +51,5 @@ docker run \
 -v /etc/kubernetes/ansible/hosts.ini:/etc/ansible/hosts \
 -v /etc/kubernetes/ansible/ansible-kubernetes-images-$K8S_VERSION-$TARGET_ARCH.tgz:/etc/ansible/linux/roles/wod.registry/files/images/ansible-kubernetes-images-$K8S_VERSION-$TARGET_ARCH.tgz \
 -w /etc/ansible/linux \
-registry.cn-qingdao.aliyuncs.com/wod/ansible-kubernetes:v1.24.7-$TARGET_ARCH \
+registry.cn-qingdao.aliyuncs.com/wod/ansible-kubernetes:$K8S_VERSION-$TARGET_ARCH \
 ansible-playbook 1.install.yml
