13c2043ae5c80732d2b3d91db000f616537cd585
shucheng <shucheng@bd-apaas.com>
2022-10-22 19:17:22 +0800

    v1.24.7 修复统信系统运行kubelet的BUG

---
+++ linux/roles/wod.kernel/files/install-kernel-UnionTech.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+
+set -e  
+
+HTTP_SERVER="${HTTP_SERVER:-https://cache.wodcloud.com/kubernetes}" 
+
+LOCAL_KERNEL=$(uname -r | head -c 3)
+LOCAL_ARCH=$(uname -m)
+
+if [ "$LOCAL_ARCH" = "x86_64" ]; then
+  TARGET_ARCH="amd64"
+elif [ "$(echo $LOCAL_ARCH | head -c 5)" = "armv8" ]; then
+  TARGET_ARCH="arm64"
+elif [ "$LOCAL_ARCH" = "aarch64" ]; then
+  TARGET_ARCH="arm64"
+elif [ "$LOCAL_ARCH" = "ppc64le" ]; then
+  TARGET_ARCH="ppc64le"
+elif [ "$LOCAL_ARCH" = "mips64" ]; then
+  TARGET_ARCH="mips64le"
+else
+  echo "This system's architecture $(LOCAL_ARCH) isn't supported"
+  TARGET_ARCH="unsupported"
+fi
+
+mkdir -p /etc/kubernetes/ansible
+
+# 统信UOS系统
