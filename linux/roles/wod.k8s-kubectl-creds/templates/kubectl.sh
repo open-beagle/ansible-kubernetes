df5364934c20e2e64495065a9030708d34d8a12f
shucheng <shucheng@bd-apaas.com>
2023-02-16 16:20:54 +0800

    v1.22.16 fix docker creds

---
+++ linux/roles/wod.k8s-kubectl-creds/templates/kubectl.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+
+set -e  
+
+REGISTRY_LOCAL="{{ REGISTRY_LOCAL }}"
+REGISTRY_REPO="{{ K8S_IMAGES['KUBECTL']['NAME'] }}"
+REGISTRY_VERSION="{{ K8S_IMAGES['KUBECTL']['VERSION'] }}{{ "-%s" | format(K8S_ARCH) | replace("-amd64", "") }}"
+
+mkdir -p /etc/kubernetes/downloads
+mkdir -p /opt/bin
+
+LOCAL_ARCH=$(uname -m)
+
+if ! [ -e /etc/kubernetes/downloads/kubectl-linux-$REGISTRY_VERSION ]; then
+  docker run -v /etc/kubernetes/downloads:/data/output \
+    --rm --entrypoint=sh \
+    $REGISTRY_LOCAL/$REGISTRY_REPO:$REGISTRY_VERSION \
+    -c 'cp /usr/local/bin/kubectl /data/output/kubectl'
+  mv /etc/kubernetes/downloads/kubectl /etc/kubernetes/downloads/kubectl-linux-$REGISTRY_VERSION
+  chmod +x /etc/kubernetes/downloads/kubectl-linux-$REGISTRY_VERSION
+  rm -rf /opt/bin/kubectl /usr/local/bin/kubectl
+  ln -s /etc/kubernetes/downloads/kubectl-linux-$REGISTRY_VERSION /opt/bin/kubectl
+  ln -s /etc/kubernetes/downloads/kubectl-linux-$REGISTRY_VERSION /usr/local/bin/kubectl
+fi
+
+/opt/bin/kubectl config set-cluster kubernetes --server=https://{{ K8S_MASTER_HOST }}:{{ K8S_MASTER_PORT }} --certificate-authority=/etc/kubernetes/ssl/ca.crt
+/opt/bin/kubectl config set-credentials default --client-certificate=/etc/kubernetes/ssl/admin.crt --client-key=/etc/kubernetes/ssl/admin.key
+/opt/bin/kubectl config set-context kubernetes --cluster=kubernetes --user=default
+/opt/bin/kubectl config use-context kubernetes  
+
+if ! (grep -q "source <(/opt/bin/kubectl completion bash)" ~/.bashrc); then
+  echo "source <(/opt/bin/kubectl completion bash)" >> ~/.bashrc
+fi
